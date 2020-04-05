c00c9c1673094ff08299e93817e0b4a62d9b9b4e
AnandBIT <56770818+AnandBIT@users.noreply.github.com>
2020-04-05 20:38:21 +0530

    new drumkit project

---
+++ index.js
@@ -0,0 +1,41 @@
+// Detecting Mouse Clicks
+for(var n=0;n<document.querySelectorAll(".drum").length;n++)
+{
+  document.querySelectorAll(".drum")[n].addEventListener("click", function(){
+  var buttonInnerHtml= this.innerHTML;
+  makeSound(buttonInnerHtml);
+}
+);
+}
+
+// Detecting Key Strokes
+document.addEventListener("keypress", function(event) { makeSound(event.key); });
+
+function makeSound(key)
+{
+  switch (key) {
+
+    case "W": case "w" : var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+    case "A": case "a": var audio=new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+    case "S": case "s": var audio=new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+    case "D": case "d": var audio=new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+    case "J": case "j": var audio=new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+    case "K": case "k": var audio=new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+    case "L": case "l": var audio=new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+  }
+}
